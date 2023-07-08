da0c63ad91695e02566c4573577a23b2f99cde50
AHKANDesire <soloniainaahkan@gmail.com>
2023-07-08 05:57:54 +0300

    connection à la base de donnée 'pharmaciegros'

--- connect.php
+++ connect.php
@@ -3,6 +3,6 @@
      $u="root";
      $pass="8510";
      $db="pharmaciegros";
-     $con=mysqli_connect($nserver,$u,$pass,$db)or die('PAS DE CONNECTION');
+     $con=mysqli_connect($nserver,$u,$pass,$db)or die('ERROR PAS DE CONNECTION');
      
 ?>
