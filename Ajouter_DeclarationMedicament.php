e5f6191344ec7925545daef2b1f6e8ff4d1d5b7d
AHKANDesire <soloniainaahkan@gmail.com>
2023-07-19 06:08:47 +0300

    Script pour insertion l'identification de medicament dans la table 'declaration_medicament'

--- Ajouter_DeclarationMedicament.php
+++ Ajouter_DeclarationMedicament.php
@@ -1,4 +1,8 @@
 <?php
+if(!(isset($_POST['Id'])))
+header('location:Page_Accueil.html');
+?>
+<?php
 include"connect.php";
 $idmed=$_POST['Id'];
 $auto=$_POST['auto'];
@@ -8,26 +12,30 @@ $per=$_POST['per'];
 $req3="INSERT INTO declaration_medicament(Id_Medicament,Num_Autorisation,Num_lot,Date_fabrication,Date_peremption) VALUES($idmed,'$auto','$lot','$fab','$per')";
 $result3=mysqli_query($con,$req3)or die('ERROR3');
 $req4="SELECT * FROM declaration_medicament WHERE Id_Medicament=$idmed";
-$result4=mysqli_query($con,$req4)or die('ERROR4');   
+$result4=mysqli_query($con,$req4)or die('ERROR4');
+$col4=mysqli_fetch_array($result4);
+mysqli_close($con);
 ?>
 <html>
     <head>
         <meta charset="utf-8">
     </head>
     <body>
+        <b> <font color='red'>IDENTIFICATION ENREGISTREE</font></b>
         <table border="1">
           <tr>
                 <th>Id_Declaration</th><th>Id_Medicament</th><th>Num_Autorisation</th><th>Num_lot</th><th>Date_fabrication</th><th>Date_peremption</th>
             </tr>
-            <?php while($col4=mysqli_fetch_array($result4)){ ?>
+           <?php while($col4=mysqli_fetch_array($result4)){ ?>
             <tr>
-                <td><?php echo"$col4[0]"; ?></td><td><?php echo"$col4[1]"; ?></td><td><?php echo"$col4[2]"; ?></td><td><?php echo"$col4[3]"; ?></td><td><?php echo"$col4[4]"; ?></td><td><?php echo"$col4[5]"; ?></td>
+                <td><?php echo"$col4[0]"; ?></td><td><?php echo"$col4[1]"; ?></td><td><?php echo"$col4[2]"; ?></td>
+                <td><?php echo"$col4[3]"; ?></td><td><?php echo"$col4[4]"; ?></td><td><?php echo"$col4[5]"; ?></td>
             </tr>
+             <?php } ?> 
             <tr>
-                <td><a href='Registe_Entrees.php?Id=<?php echo"$col4[0]"; ?>'>SUIVANT</a></td>
+                <td>INSERER DANS REGISTRE ENTREES:
+                <a href='Registre_Entrees.php?Id=<?php echo"$idmed"; ?>'>SUIVANT</a></td>
             </tr>
-            <?php } ?>
         </table>
     </body>
 </html>
-<?php mysqli_close($con); ?>
