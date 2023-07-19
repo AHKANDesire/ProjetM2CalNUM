5a9267531a7e77d2145e0f9252c29d3e5c863361
AHKANDesire <soloniainaahkan@gmail.com>
2023-07-19 05:57:06 +0300

    Script pour insertion la nomination de medicament dans la table 'listemedicament'

--- Ajouter_ListeMedicament.php
+++ Ajouter_ListeMedicament.php
@@ -1,6 +1,6 @@
 <?php
 if(!(isset($_POST['dci'])))
-header('location:FormMedicament.html');
+header('location:Page_Accueil.html');
 ?>
 <?php
 include"connect.php";
@@ -8,42 +8,47 @@ $dci=$_POST['dci'];
 $forme=$_POST['forme'];
 $dosage=$_POST['dosage'];
 $cate=$_POST['cate'];
-$req="SELECT Denomination,Forme,Dosage FROM listemedicament";
+$sma=$_POST['sma'];
+$smi=$_POST['smi'];
+$obs=$_POST['obs'];
+$req="SELECT * FROM listemedicament";
 $result=mysqli_query($con,$req)or die('ERROR');
             WHILE($col=mysqli_fetch_array($result))
             {
                 if($col['Denomination']==$dci and $col['Forme'] ==$forme and $col['Dosage']==$dosage)
                 {
-                 echo"<font color='red'>IL EST DEJA DANS LA LISTE</font>"."<br>";               
-                 echo"AFFICHER LA LISTE cliquez: ";
-                 echo"<a href='Afficher_ListeMedicament.php'>LISTE </a> ";              
+                 echo"<font color='red'>IL EST DEJA DANS LA LISTE</font>"."<br>";
+                 echo"<b>IDENTIFICATION:</b><a href='Declaration_Medicament.php?Id=$col[0]'>SUIVANT</a> ";              
                  mysqli_close($con);
                  exit();                 
                 }
             }
-$req1="INSERT INTO listemedicament(Denomination,Forme,Dosage,Categorie)VALUES('$dci','$forme','$dosage','$cate')";
+$req1="INSERT INTO listemedicament(Denomination,Forme,Dosage,Categorie,Stock_max,Stock_min,Observation)VALUES('$dci','$forme','$dosage','$cate',$sma,$smi,'$obs')";
 $result1=mysqli_query($con,$req1)or die('ERROR1');
 $req2="SELECT * FROM listemedicament WHERE Denomination='$dci' AND Dosage='$dosage'";
-$result2=mysqli_query($con,$req2)or die('ERROR2');   
+$result2=mysqli_query($con,$req2)or die('ERROR2');
+$col2=mysqli_fetch_array($result2);
+mysqli_close($con);
 ?>
 <html>
     <head>
         <meta charset="utf-8">
     </head>
     <body>
+        <b><font color='red'>NOMINATION ENREGISTREE</font></b>
         <table border="1">
-          <tr>
-                <th>Id_Medicament</th><th>DCI</th><th>Forme</th><th>Dosage</th>
+            <tr>
+                <th>Id_Medicament</th><th>DCI</th><th>Forme</th><th>Dosage</th><th>Catégorie</th><th>Stock_max</th><th>Stock_min</th><th>Observation</th>
             </tr>
-            <?php while($col2=mysqli_fetch_array($result2)){ ?>
             <tr>
                 <td><?php echo"$col2[0]"; ?></td><td><?php echo"$col2[1]"; ?></td><td><?php echo"$col2[2]"; ?></td><td><?php echo"$col2[3]"; ?></td>
+                <td><?php echo"$col2[4]"; ?></td><td><?php echo"$col2[5]"; ?></td><td><?php echo"$col2[6]"; ?></td><td><?php echo"$col2[7]"; ?></td>
             </tr>
             <tr>
-                <td><a href='Declaration_Medicament.php?Id=<?php echo"$col2[0]"; ?>'>SUIVANT</a></td>
+                <td><b>IDENTIFICATION:</b>
+                <a href='Declaration_Medicament.php?Id=<?php echo"$col2[0]"; ?>'>SUIVANT</a></td>
             </tr>
-            <?php } ?>
+            
         </table>
     </body>
 </html>
-<?php mysqli_close($con); ?>
