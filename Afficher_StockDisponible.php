bd77e783dc27d02eb9e5ae99f3a575c66a404914
AHKANDesire <soloniainaahkan@gmail.com>
2023-07-22 23:34:39 +0300

    Pour afficher les noms et les stocks disponibles de medicament dans la registre de Stock en relation avec la liste de medicament

---
+++ Afficher_StockDisponible.php
@@ -0,0 +1,28 @@
+<?php
+include"connect.php";
+$req="SELECT Id_stock,Date_stock,Denomination,Forme,Dosage,Stock_disponible,Prix_U,Prix_T FROM registre_stock INNER JOIN listemedicament ON registre_stock.Id_medicament=listemedicament.Id_Medicament";
+$result=mysqli_query($con,$req)or die('ERROR');
+?>
+ <html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+     <h2> <font color='green'>LE STOCK DISPONIBLE DE MEDICAMENTS</font></h2>
+        <table border="1">
+            <tr>
+                <th>Id_stock</th><th>Date_stock</th><th>DCI</th><th>Forme</th><th>Dosage</th><th>Stock Disponible</th><th>Prix Unitaire</th><th>Prix Total</th>
+            </tr>
+            <?php while($col=mysqli_fetch_array($result)){ ?>
+            <tr>
+                <td><?php echo"$col[0]"; ?></td><td><?php echo"$col[1]"; ?></td><td><?php echo"$col[2]"; ?></td><td><?php echo"$col[3]"; ?></td>
+                <td><?php echo"$col[4]"; ?></td><td><?php echo"$col[5]"; ?></td><td><?php echo"$col[6]"; ?></td><td><?php echo"$col[7]"; ?></td>
+            </tr>
+            <?php } mysqli_close($con); ?>
+            <tr>
+                <th>RETOUR A LA PAGE D'ACCUEIL
+                 <a href='Page_Accueil.html'>RETOUR</a></th>
+            </tr>
+        </table>
+    </body>
+</html>:
