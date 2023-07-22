689afb4507652f5f468a120af5a0d5198a4b7415
AHKANDesire <soloniainaahkan@gmail.com>
2023-07-22 23:13:02 +0300

    Pour afficher la Registre de Stock de medicaments

---
+++ Afficher_ListeStock.php
@@ -0,0 +1,30 @@
+<?php
+include"connect.php";
+$req="SELECT * FROM registre_stock";
+$result=mysqli_query($con,$req)or die('ERROR');
+?>
+ <html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+     <h2> <font color='purple'>LA REGISTRE DE STOCK DE MEDICAMENTS</font></h2>
+        <table border="1">
+            <tr>
+                <th>Id_stock</th><th>Date_stock</th><th>Id_medicament</th><th>Stock_disponible</th><th>Prix_U</th><th>Prix_T</th><th>Observation</th>
+            </tr>
+            <?php while($col=mysqli_fetch_array($result)){ ?>
+              <tr>
+                <td><?php echo"$col[0]"; ?></td><td><?php echo"$col[1]"; ?></td><td><?php echo"$col[2]"; ?></td><td><?php echo"$col[3]"; ?></td>
+                <td><?php echo"$col[4]"; ?></td><td><?php echo"$col[5]"; ?></td><td><?php echo"$col[6]"; ?></td>
+                <td><a href='SupprimerListeStock.php?Id=<?php echo"$col[0]"; ?>'>SUPPRIMER</a></td>
+                <td><a href='EditerListeStock.php?Id=<?php echo"$col[0]"; ?>'>EDITER</a></td>
+              </tr>
+            <?php } mysqli_close($con); ?>
+            <tr>
+                <th>RETOUR A LA PAGE D'ACCUEIL
+                 <a href='Page_Accueil.html'>RETOUR</a></th>
+            </tr>
+        </table>
+    </body>
+</html>:
