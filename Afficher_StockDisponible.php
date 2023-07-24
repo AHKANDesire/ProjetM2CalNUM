313a28172b599c3fbd98984b3ef8a043abf49076
AHKANDesire <soloniainaahkan@gmail.com>
2023-07-24 16:05:44 +0300

    Fichier pour afficher tous les stocks de medicaments disponibles

--- Afficher_StockDisponible.php
+++ Afficher_StockDisponible.php
@@ -1,6 +1,6 @@
 <?php
 include"connect.php";
-$req="SELECT Id_stock,Date_stock,Denomination,Forme,Dosage,Stock_disponible,Prix_U,Prix_T FROM registre_stock INNER JOIN listemedicament ON registre_stock.Id_medicament=listemedicament.Id_Medicament";
+$req="SELECT Id_stock,Date_stock,Denomination,Forme,Dosage,Stock_disponible,Prix_U,Prix_T,Stock_max,Stock_min FROM registre_stock INNER JOIN listemedicament ON registre_stock.Id_medicament=listemedicament.Id_Medicament";
 $result=mysqli_query($con,$req)or die('ERROR');
 ?>
  <html>
@@ -12,11 +12,13 @@ $result=mysqli_query($con,$req)or die('ERROR');
         <table border="1">
             <tr>
                 <th>Id_stock</th><th>Date_stock</th><th>DCI</th><th>Forme</th><th>Dosage</th><th>Stock Disponible</th><th>Prix Unitaire</th><th>Prix Total</th>
+                <th>Stock maximal</th><th>Stock minimal</th>
             </tr>
             <?php while($col=mysqli_fetch_array($result)){ ?>
             <tr>
                 <td><?php echo"$col[0]"; ?></td><td><?php echo"$col[1]"; ?></td><td><?php echo"$col[2]"; ?></td><td><?php echo"$col[3]"; ?></td>
                 <td><?php echo"$col[4]"; ?></td><td><?php echo"$col[5]"; ?></td><td><?php echo"$col[6]"; ?></td><td><?php echo"$col[7]"; ?></td>
+                <td><?php echo"$col[8]"; ?></td><td><?php echo"$col[9]"; ?></td>
             </tr>
             <?php } mysqli_close($con); ?>
             <tr>
@@ -25,4 +27,4 @@ $result=mysqli_query($con,$req)or die('ERROR');
             </tr>
         </table>
     </body>
-</html>:
+</html>
