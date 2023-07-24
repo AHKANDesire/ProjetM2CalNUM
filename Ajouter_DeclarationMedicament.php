8d8d383eee896ebabd658d917a5373cd74287ef8
AHKANDesire <soloniainaahkan@gmail.com>
2023-07-24 18:57:41 +0300

    Fichier pour ajouter dans la base de donnée l'identification de medicament notament date de Peremption

--- Ajouter_DeclarationMedicament.php
+++ Ajouter_DeclarationMedicament.php
@@ -13,25 +13,23 @@ $req3="INSERT INTO declaration_medicament(Id_Medicament,Num_Autorisation,Num_lot
 $result3=mysqli_query($con,$req3)or die('ERROR3');
 $req4="SELECT * FROM declaration_medicament WHERE Id_Medicament=$idmed";
 $result4=mysqli_query($con,$req4)or die('ERROR4');
-$col4=mysqli_fetch_array($result4);
-mysqli_close($con);
 ?>
 <html>
     <head>
         <meta charset="utf-8">
     </head>
     <body>
-        <b> <font color='red'>IDENTIFICATION ENREGISTREE</font></b>
+        <b> <font color='green'>IDENTIFICATION ENREGISTREE</font></b>
         <table border="1">
           <tr>
-                <th>Id_Declaration</th><th>Id_Medicament</th><th>Num_Autorisation</th><th>Num_lot</th><th>Date_fabrication</th><th>Date_peremption</th>
+                <th>Id_identification</th><th>Id_Medicament</th><th>Num_Autorisation</th><th>Num_lot</th><th>Date_fabrication</th><th>Date_peremption</th>
             </tr>
            <?php while($col4=mysqli_fetch_array($result4)){ ?>
             <tr>
                 <td><?php echo"$col4[0]"; ?></td><td><?php echo"$col4[1]"; ?></td><td><?php echo"$col4[2]"; ?></td>
                 <td><?php echo"$col4[3]"; ?></td><td><?php echo"$col4[4]"; ?></td><td><?php echo"$col4[5]"; ?></td>
             </tr>
-             <?php } ?> 
+             <?php }  mysqli_close($con);?>
             <tr>
                 <td>INSERER DANS REGISTRE ENTREES:
                 <a href='Registre_Entrees.php?Id=<?php echo"$idmed"; ?>'>SUIVANT</a></td>
