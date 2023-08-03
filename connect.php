0b9134d326523900da47eebef2eac03f4f5308d6
AHKANDesire <soloniainaahkan@gmail.com>
2023-08-03 17:40:15 +0300

    Fichier pour connecter à la base de donnée

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-     $nserver="localhost";
-     $u="root";
-     $pass="8510";
-     $db="pharmaciegros";
+     $nserver="sql307.infinityfree.com";
+     $u="if0_34692506";
+     $pass="V5bIpoFhWYKjJ";
+     $db="if0_34692506_pharmaciegros";
      $con=mysqli_connect($nserver,$u,$pass,$db)or die('ERROR PAS DE CONNECTION');
      
 ?>
